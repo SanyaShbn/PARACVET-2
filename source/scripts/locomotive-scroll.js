1ae8a3a64b8d1f342a02ec060844c43619e98554
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2025-06-14 16:11:44 +0300

    Migrated locomotive-scroll back to beta v5

--- source/scripts/locomotive-scroll.js
+++ source/scripts/locomotive-scroll.js
@@ -1,27 +1,19 @@
-import LocomotiveScroll from 'locomotive-scroll';
+import LocomotiveScroll from "locomotive-scroll";
 
-let scroll;
+const locomotiveScroll = new LocomotiveScroll({
+    lenisOptions: {
+        wrapper: window,
+        content: document.documentElement,
+        smoothWheel: true,
+        smoothTouch: false,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        normalizeWheel: true,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    },
+});
 
-function initLocomotiveScroll() {
-    const scrollContainer = document.querySelector('[data-scroll-container]');
-    
-    if (!scrollContainer) {
-        return;
-    }
-
-    if (scroll) {
-        scroll.destroy();
-    }
-
-    scroll = new LocomotiveScroll({
-        el: scrollContainer,
-        smooth: true,
-        multiplier: 1,
-    });
-}
-
-document.addEventListener("DOMContentLoaded", initLocomotiveScroll);
-
-document.addEventListener("astro:page-load", () => {
-    initLocomotiveScroll();
+window.addEventListener("load", () => {
+  locomotiveScroll.scrollTo("top", { duration: 0 }); // пока так отматываю скролл обратно вверх при перезагрузке страницы, 
+                                                     // при навигации вроде и так в начало сбрасывается
 });
